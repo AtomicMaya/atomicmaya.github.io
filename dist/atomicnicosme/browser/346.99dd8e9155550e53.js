bab123e36b2a92d263c8d24c0e6e94a228a3a983
AtomicNicos <atomicnicos@gmail.com>
2022-09-11 16:25:57 +0200

    STANDARDIZED COMMIT: GH-PAGES

---
+++ dist/atomicnicosme/browser/346.99dd8e9155550e53.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkatomicnicosme=self.webpackChunkatomicnicosme||[]).push([[346],{346:(c,t,s)=>{s.r(t),s.d(t,{Posts2022RoutingModule:()=>a});var u=s(3180),l=s(5420),n=s(4650);let a=(()=>{class o{}return o.\u0275fac=function(P){return new(P||o)},o.\u0275mod=n.oAB({type:o}),o.\u0275inj=n.cJS({imports:[u.Bz.forChild(l.q),u.Bz]}),o})()}}]);
