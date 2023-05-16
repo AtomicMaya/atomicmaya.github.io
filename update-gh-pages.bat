49a242318863ae34b3ca09203c507a1a0c873159
AtomicMaya <atomicmaya@pm.me>
2023-05-16 12:27:17 +0200

    tweak update script

--- update-gh-pages.bat
+++ update-gh-pages.bat
@@ -3,4 +3,4 @@ npm run prerender
 git add .
 git commit -m "STANDARDIZED COMMIT: GH-PAGES"
 git push -u origin main
-git subtree push --prefix dist/digital-overdose/browser/ origin gh-pages
+git subtree push --prefix dist/atomicmayame/browser/ origin gh-pages
