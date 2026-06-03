692def570d30b6d9128a4f15c26babc14ba64eab
maya | 551724 <maya@catgirl.global>
2026-06-03 17:45:23 +0200

    almost at the end of 2022 (we know what happens then)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,61 +3,66 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import rehypeAstroRelativeMarkdownLinks from "astro-rehype-relative-markdown-links";
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders, sharpImageService } from 'astro/config';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
+import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://atomicmaya.me',
-	integrations: [mdx(), sitemap()],
-	build: {
-		assets: 'assets'
-	},
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
-	markdown: {
-		rehypePlugins: [
-			rehypeSlug,
-			rehypeAstroRelativeMarkdownLinks,
-			[
-				rehypeAutolinkHeadings,
-				{
-					behavior: 'prepend',
-					content: {
-						type: 'text',
-						value: '',
-					},
-					headingProperties: {
-						className: ['anchor'],
-					},
-					properties: {
-						className: ['anchor-link'],
-					},
-				},
-			],
-		],
-	},
+    site: 'https://atomicmaya.me',
+    integrations: [expressiveCode(), mdx(), sitemap(), pluginLineNumbers()],
+    build: {
+        assets: 'assets'
+    },
+    image: {
+        service: sharpImageService({ limitInputPixels: false }),
+    },
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: 'Atkinson',
+            cssVariable: '--font-atkinson',
+            fallbacks: ['sans-serif'],
+            options: {
+                variants: [
+                    {
+                        src: ['./src/assets/fonts/atkinson-regular.woff'],
+                        weight: 400,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                    {
+                        src: ['./src/assets/fonts/atkinson-bold.woff'],
+                        weight: 700,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                ],
+            },
+        },
+    ],
+    markdown: {
+        rehypePlugins: [
+            rehypeSlug,
+            rehypeAstroRelativeMarkdownLinks,
+            [
+                rehypeAutolinkHeadings,
+                {
+                    behavior: 'prepend',
+                    content: {
+                        type: 'text',
+                        value: '',
+                    },
+                    headingProperties: {
+                        className: ['anchor'],
+                    },
+                    properties: {
+                        className: ['anchor-link'],
+                    },
+                },
+            ],
+        ],
+    },
 });
--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,6 +2,7 @@ export interface SocialsLink {
 	label: string;
 	icon: string;
 	url: string;
+	aria?: string;
 }
 
 export const socials: SocialsLink[] = [
@@ -9,16 +10,19 @@ export const socials: SocialsLink[] = [
 		label: 'Blog',
 		icon: 'fas fa-blog fa-2x',
 		url: 'https://atomicmaya.me',
+		aria: 'Blog',
 	},
 	{
 		label: 'Fediverse',
-		icon: 'fab fa-mastodon fa-2x',
+		icon: 'lolf lol-fedi fa-2x fab fa-2x',
 		url: 'https://catgirl.global/@maya',
+		aria: 'Fediverse',
 	},
 	{
 		label: 'Photography',
 		icon: 'fas fa-camera-retro fa-2x',
 		url: 'https://pixels.catgirl.global/@maya',
+		aria: 'Camera',
 	},
 	/*{
 		label: 'Twitter',
@@ -29,6 +33,7 @@ export const socials: SocialsLink[] = [
 		label: 'LinkedIn',
 		icon: 'fab fa-linkedin fa-2x',
 		url: 'https://linkedin.com/in/maya-boeckh',
+		aria: 'Linkedin',
 	},
 	/*{
 		label: 'Keybase',
@@ -38,16 +43,19 @@ export const socials: SocialsLink[] = [
 	{
 		label: 'Bluesky',
 		icon: 'lolf lol-bluesky fab fa-2x',
-		url: 'https://bsky.app/profile/atomicmaya.me'
+		url: 'https://bsky.app/profile/atomicmaya.me',
+		aria: 'Bluesky',
 	},
 	{
 		label: 'GitHub',
 		icon: 'fab fa-github fa-2x',
 		url: 'https://github.com/AtomicMaya',
+		aria: 'Github',
 	},
 	{
 		label: 'RSS',
 		icon: 'fas fa-rss-square fa-2x',
-		url: 'https://atomicmaya.me/rss.xml'
+		url: 'https://atomicmaya.me/rss.xml',
+		aria: 'RSS',
 	}
 ]
