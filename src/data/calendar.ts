7f936e4f2564e5f7250a4f2aae1ae02662cf65ca
maya | 551724 <maya@catgirl.global>
2026-06-12 14:24:02 +0200

    new

--- src/data/calendar.ts
+++ src/data/calendar.ts
@@ -6,8 +6,9 @@ export interface CalendarItem {
 }
 
 export const CalendarData: CalendarItem[] = [
-	{eventName: "Electromagnetic Field", location: "Eastnor Castle Deer Park, United Kingdom", startDate: new Date("2026-07-16T02:00:00"), endDate: new Date("2026-07-19T02:00:00")},
-	{eventName: "GPN2026", location: "Karlsruhe, Germany", startDate: new Date("2026-06-04T02:00:00"), endDate: new Date("2026-06-07T02:00:00")},
+	{eventName: "Hack Glasgow", location: "Glasgow, Scotland", startDate: new Date("2026-08-15T02:00:00")},
+	{eventName: "Electromagnetic Fields (EMF 2026)", location: "Eastnor Castle Deer Park, United Kingdom", startDate: new Date("2026-07-16T02:00:00"), endDate: new Date("2026-07-19T02:00:00")},
+	{eventName: "CoSin", location: "Biel/Bienne, Switzerland", startDate: new Date("2026-06-12T15:00:00"), endDate: new Date("2026-06-14T13:00:00")},
 	{eventName: "Easterhegg 23", location: "Koblenz, Germany", startDate: new Date("2026-04-03T02:00:00"), endDate: new Date("2026-04-06T02:00:00")},
 	{eventName: "83. Kontiki Konferenz", location: "Berlin, Germany", startDate: new Date("2026-03-11T02:00:00"), endDate: new Date("2026-03-12T02:00:00")},
 	{eventName: "MountainBytes", location: "Steinhausen, Switzerland", startDate: new Date("2026-02-27T02:00:00"), endDate: new Date("2026-03-01T02:00:00")},
@@ -28,6 +29,6 @@ export const CalendarData: CalendarItem[] = [
 	{eventName: "BSides Newcastle", location: "Newcastle, United Kingdom", startDate: new Date("2024-10-05T02:00:00")},
 	{eventName: "DEFCON 32", location: "Las Vegas, United States", startDate: new Date("2024-08-08T02:00:00"), endDate: new Date("2024-08-11T02:00:00")},
 	{eventName: "SteelCon", location: "Sheffield, United Kingdom", startDate: new Date("2024-07-19T02:00:00"), endDate: new Date("2024-07-21T02:00:00")},
-	{eventName: "Electromagnetic Field", location: "Eastnor Castle Deer Park, United Kingdom", startDate: new Date("2024-05-25T02:00:00"), endDate: new Date("2024-06-03T02:00:00")},
+	{eventName: "Electromagnetic Fields (EMF 2024)", location: "Eastnor Castle Deer Park, United Kingdom", startDate: new Date("2024-05-25T02:00:00"), endDate: new Date("2024-06-03T02:00:00")},
 	{eventName: "BSides Cymru", location: "Cardiff, Wales", startDate: new Date("2024-04-27T02:00:00")}
 ];
